27ffd4fd8b9bd5719b7aa63347ef9ccbf7627559
Reinis Riekstins <riekstins.reinis@gmail.com>
2017-03-29 14:58:23 +0300

    updated post thumnail a bit

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ $('.row.small-up-1.medium-up-2.large-up-3').html(function() {
       <div class="column">
         <div class="post-thumbnail">
           <a href="#">
-            <img src="https://placehold.it/400x300" alt="">
-            <h3>Lorem ipsum dolor sit amet, consectetur.</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore possimus dolorem maxime, nisi.</p>
+            <img src="https://placehold.it/400x300?text=Picture" alt="">
+            <h5>Lorem ipsum dolor sit amet, consectetur.</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore possimus dolorem maxime, nisi...</p>
           </a>
         </div>
       </div>`
