36b2fce899bbd228602577e21013e29908a1c8d8
Reinis Riekstins <riekstins.reinis@gmail.com>
2017-04-05 14:33:22 +0300

    put img inside figure, to make the zoomed image not become larger on hover

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import './index.scss'
 import $ from 'jquery'
 
-$('.row.small-up-1.medium-up-2.large-up-3').html(function() {
+//****************
+// Index Page
+//****************
+$('.row.small-up-1.medium-up-2.large-up-3').append(() => {
   let content = ''
   for (let i = 0; i < 12; i++) {
     content += `
       <div class="column">
         <div class="post-thumbnail">
           <a href="#">
-            <img src="https://placehold.it/400x300?text=Picture" alt="">
+            <figure>
+              <img src="https://placehold.it/1200x900?text=Picture" alt="">
+            </figure>
             <h5>Lorem ipsum dolor sit amet, consectetur.</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore possimus dolorem maxime, nisi...</p>
           </a>
